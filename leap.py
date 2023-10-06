bb95d2a9965c3913f8f1cf50a09d0ee6dee7945f
Munjuri <118501842+Munjuri@users.noreply.github.com>
2023-10-06 09:39:59 +0300

    leap.py

---
+++ leap.py
@@ -0,0 +1,12 @@
+#Allan Munjuri SCT211-0624/2022
+
+print("Enter Year: ")
+year = int(input())
+result = year % 4
+
+if result == 0:
+  print("This is a leap year")
+
+else: 
+  print("This is not a leap year")
+  
